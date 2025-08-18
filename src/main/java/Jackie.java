ac6aa44c521412a1331cdece369a73d472b40a63
qiiiixuan <leleqx123@gmail.com>
2025-08-19 00:08:18 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Jackie.java
@@ -0,0 +1,14 @@
+public class Jackie {
+    public static void main(String[] args) {
+        String logo = """
+                 _____           _
+                |__ __|         | |    0
+                  | |/ _ ` /   \\| |/ /| |/   \\
+                 _| | |_| |   < |  <  | |  __/
+                \\___/\\__,_|\\___/|_|\\_\\|_|\\___/
+                """;
+        System.out.println("Welcome! I am...\n" + logo + "______________________________");
+        System.out.println("Ask me anything!\n");
+        System.out.println("______________________________\nGoodbye!");
+    }
+}

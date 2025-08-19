49acb94177a588122b38d66a41474d85ef1c3870
qiiiixuan <leleqx123@gmail.com>
2025-08-19 15:26:33 +0800

    Echo

--- src/main/java/Jackie.java
+++ src/main/java/Jackie.java
@@ -1,14 +1,23 @@
+import java.util.*;
+
 public class Jackie {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String logo = """
                  _____           _
-                |__ __|         | |    0
-                  | |/ _ ` /   \\| |/ /| |/   \\
-                 _| | |_| |   < |  <  | |  __/
+                |__ __|__   ___ | | __ *  ___
+                  | |/ _  |/ ,_\\| |/ /| |/ _ \\
+                ,_| | |_| |  |_ |   < | |  __/
                 \\___/\\__,_|\\___/|_|\\_\\|_|\\___/
                 """;
+        Scanner sc = new Scanner(System.in);
+        String input = "";
         System.out.println("Welcome! I am...\n" + logo + "______________________________");
         System.out.println("Ask me anything!\n");
+        input = sc.nextLine();
+        while(!Objects.equals(input, "bye")) {
+            System.out.println("\t" + input + "\n");
+            input = sc.nextLine();
+        }
         System.out.println("______________________________\nGoodbye!");
     }
 }

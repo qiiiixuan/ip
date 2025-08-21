764b65eebe03462542ef77eadce077d247895a9f
qiiiixuan <leleqx123@gmail.com>
2025-08-21 14:34:34 +0800

    Add, List

--- src/main/java/Jackie.java
+++ src/main/java/Jackie.java
@@ -10,12 +10,21 @@ public class Jackie {
                 \\___/\\__,_|\\___/|_|\\_\\|_|\\___/
                 """;
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> history = new ArrayList<>(List.of());
         String input = "";
         System.out.println("Welcome! I am...\n" + logo + "______________________________");
         System.out.println("Ask me anything!\n");
         input = sc.nextLine();
         while(!Objects.equals(input, "bye")) {
-            System.out.println("\t" + input + "\n");
+            if (Objects.equals(input, "list")) {
+                for (int i = 0; i < history.size(); i++) {
+                    System.out.println("\t" + (i + 1) + ". " + history.get(i));
+                }
+                System.out.println();
+            } else {
+                history.add(input);
+                System.out.println("\tadded: " + input + "\n");
+            }
             input = sc.nextLine();
         }
         System.out.println("______________________________\nGoodbye!");
